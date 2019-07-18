f4207b2f1c00c7e00307b159fd0a105f1d35bc84
antoniobentosela <52534695+antoniobentosela@users.noreply.github.com>
2019-07-18 12:14:57 -0300

    asd

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,49 +1,41 @@
 function mostrar()
 {
-    var numero1
-    var numero2
-    var resultado
+  var numero1
+  var numero2
+  var resultado
 
 
-        numero1 = prompt ("Escriba un numero");
-        numero2 = prompt ("Escriba otro numero");
-            
-            if (numero1 == numero2) {
+    numero1 = prompt ("Escriba un numero");
+    numero2 = prompt ("Escriba otro numero");
 
-                alert (numero1 + numero2);
-          
-            } else {
+      if (numero1 == numero2) {
 
-                if (numero1 > numero2){
+        alert (numero1 + numero2);
 
-                    numero1 = parseInt (numero1);
-                    numero2 = parseInt (numero2);
+      } else {
 
-                        alert (numero1 - numero2);
-                } else { 
-                    
-                        if (numero1 < numero2){
-                       
-                        numero1 = parseInt (numero1);
-                        numero2 = parseInt (numero2);
-                            resultado = parseInt (resultado);
-                          
-                                resultado = alert (numero1 + numero2);
-   
-                    } else {
+        if (numero1 > numero2){
 
-                        resultado = parseInt (resultado);
-                            
-                            if (resultado > 10){
+          numero1 = parseInt (numero1);
+          numero2 = parseInt (numero2);
 
-                            alert ("hols");
-                        }
-                    }
+            alert (numero1 - numero2);
 
-                        
+      } else {
 
-                        
-                     
-                }   
-            }  
+        if (numero1 < numero2){
+
+          numero1 = parseInt (numero1);
+          numero2 = parseInt (numero2);
+
+          alert (numero1 + numero2);
+
+          if (numero1 + numero2 > 10){
+
+            alert ("la suma es " + );
+          }
+          }
+      }
+      }
 }
+
