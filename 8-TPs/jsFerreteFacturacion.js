d12677c46e0fea44856cefb401d02a503024c83d
antoniobentosela <52534695+antoniobentosela@users.noreply.github.com>
2019-07-18 01:16:57 -0300

    asd

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,46 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var precio1
+var precio2
+var precio3
+
 function Sumar () 
 {
-	
+    precio1 = document.getElementById ("PrecioUno").value;
+    precio2 = document.getElementById ("PrecioDos").value;
+    precio3 = document.getElementById ("PrecioTres").value;
+
+    precio1 = parseInt (precio1);
+    precio2 = parseInt (precio2);
+    precio3 = parseInt (precio3);
+    
+        alert (precio1 + precio2 + precio3);
+    
 }
 function Promedio () 
 {
+    precio1 = document.getElementById ("PrecioUno").value;
+    precio2 = document.getElementById ("PrecioDos").value;
+    precio3 = document.getElementById ("PrecioTres").value;
+
+    precio1 = parseInt (precio1);
+    precio2 = parseInt (precio2);
+    precio3 = parseInt (precio3);
+
+        alert ((precio1 + precio2 + precio3) / 3);
 	
 }
 function PrecioFinal () 
 {
+    precio1 = document.getElementById ("PrecioUno").value;
+    precio2 = document.getElementById ("PrecioDos").value;
+    precio3 = document.getElementById ("PrecioTres").value;
+
+    precio1 = parseInt (precio1);
+    precio2 = parseInt (precio2);
+    precio3 = parseInt (precio3);
+
+        alert ((precio1 + precio2 + precio3) / 21 * 100);
 	
 }
