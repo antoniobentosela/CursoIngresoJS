32d348ad37bc52460affd14828028c9bcf904ac5
antoniobentosela <52534695+antoniobentosela@users.noreply.github.com>
2019-08-10 13:04:30 -0300

    Add files via upload

--- 2019_2Cuatri_Parcial_fuentes/tres.js
+++ 2019_2Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,40 @@
 function mostrar()
 {
-	alert("tres");
+	var localidad;
+	var habitantes;
+	var respuesta = true;
+	var temperatura;
+	var contadorPar;
+	
+	while (respuesta){
+
+		localidad = prompt ("Ingrese la localidad en la que vive");
+		
+		habitantes = prompt ("Ingrese la cantidad de habitantes de su localidad");
+		
+		while (isNaN (habitantes) == true || habitantes > 40 || habitantes < 1){ 
+
+			habitantes = prompt ("Error: Ingrese una cantidad entre 1 y 40");
+
+		} 
+
+		temperatura = prompt ("Ingrese la temperatura minima de su localidad");
+
+		while (isNaN (temperatura) == true || temperatura > 50 || temperatura < -50){ 
+			
+			temperatura = prompt ("Error: Ingrese una temperatura entre -50 y 50");
+			
+		} 
+
+
+			if (temperatura % 2 == 0) {
+				contadorPar ++;
+			} // esto es para calcular los pares. 
+
+			
+
+
+		respuesta = confirm ("Desea continuar?");
+	}
+
 }
